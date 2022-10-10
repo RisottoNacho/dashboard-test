82b7e4216680740eec273f095fee5f1275ddbada
Ignacio López Ares <47207829+RisottoNacho@users.noreply.github.com>
2022-10-10 16:52:33 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './main.css';
+import { Elm } from './Dash.elm';
+
+Elm.Dash.init({
+  node: document.getElementById('elm-node')
+});
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
