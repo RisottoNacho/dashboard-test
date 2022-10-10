f9b56719db03c8cb3d3629405cee7e10a4128367
Ignacio López Ares <47207829+RisottoNacho@users.noreply.github.com>
2022-10-10 17:04:20 +0200

    Update Dash.elm

--- src/Dash.elm
+++ src/Dash.elm
@@ -3,7 +3,7 @@ module Dash exposing (..)
 import Browser
 import Browser.Events as E
 import Html exposing (Html)
-import Html.Attributes exposing (width, height, style)
+import Html.Attributes exposing (height, style, width)
 import Math.Matrix4 as Mat4 exposing (Mat4)
 import Math.Vector3 as Vec3 exposing (Vec3, vec3)
 import WebGL
@@ -14,12 +14,12 @@ import WebGL
 
 
 main =
-  Browser.element
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -27,12 +27,12 @@ main =
 
 
 type alias Model =
-  Float
+    Float
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init () =
-  ( 0, Cmd.none )
+    ( 0, Cmd.none )
 
 
 
@@ -40,14 +40,14 @@ init () =
 
 
 type Msg
-  = TimeDelta Float
+    = TimeDelta Float
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg angle =
-  case msg of
-    TimeDelta dt ->
-      ( angle + dt / 5000, Cmd.none )
+    case msg of
+        TimeDelta dt ->
+            ( angle + dt / 5000, Cmd.none )
 
 
 
@@ -56,7 +56,7 @@ update msg angle =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  E.onAnimationFrameDelta TimeDelta
+    E.onAnimationFrameDelta TimeDelta
 
 
 
@@ -65,29 +65,31 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view angle =
-  WebGL.toHtml
-    [ width 400, height 400, style "display" "block"
-    ]
-    [ WebGL.entity vertexShader fragmentShader cubeMesh (uniforms angle)
-    ]
+    WebGL.toHtml
+        [ width 400
+        , height 400
+        , style "display" "block"
+        ]
+        [ WebGL.entity vertexShader fragmentShader cubeMesh (uniforms angle)
+        ]
 
 
 type alias Uniforms =
-  { rotation : Mat4
-  , perspective : Mat4
-  , camera : Mat4
-  }
+    { rotation : Mat4
+    , perspective : Mat4
+    , camera : Mat4
+    }
 
 
 uniforms : Float -> Uniforms
 uniforms angle =
-  { rotation =
-      Mat4.mul
-        (Mat4.makeRotate (3 * angle) (vec3 0 1 0))
-        (Mat4.makeRotate (2 * angle) (vec3 1 0 0))
-  , perspective = Mat4.makePerspective 45 1 0.01 100
-  , camera = Mat4.makeLookAt (vec3 0 0 5) (vec3 0 0 0) (vec3 0 1 0)
-  }
+    { rotation =
+        Mat4.mul
+            (Mat4.makeRotate (3 * angle) (vec3 0 1 0))
+            (Mat4.makeRotate (2 * angle) (vec3 1 0 0))
+    , perspective = Mat4.makePerspective 45 1 0.01 100
+    , camera = Mat4.makeLookAt (vec3 0 0 5) (vec3 0 0 0) (vec3 0 1 0)
+    }
 
 
 
@@ -95,42 +97,58 @@ uniforms angle =
 
 
 type alias Vertex =
-  { color : Vec3
-  , position : Vec3
-  }
+    { color : Vec3
+    , position : Vec3
+    }
 
 
 cubeMesh : WebGL.Mesh Vertex
 cubeMesh =
-  let
-    rft = vec3 1 1 1
-    lft = vec3 -1 1 1
-    lbt = vec3 -1 -1 1
-    rbt = vec3 1 -1 1
-    rbb = vec3 1 -1 -1
-    rfb = vec3 1 1 -1
-    lfb = vec3 -1 1 -1
-    lbb = vec3 -1 -1 -1
-  in
-  WebGL.triangles <| List.concat <|
-    [ face (vec3 115 210 22 ) rft rfb rbb rbt -- green
-    , face (vec3 52  101 164) rft rfb lfb lft -- blue
-    , face (vec3 237 212 0  ) rft lft lbt rbt -- yellow
-    , face (vec3 204 0   0  ) rfb lfb lbb rbb -- red
-    , face (vec3 117 80  123) lft lfb lbb lbt -- purple
-    , face (vec3 245 121 0  ) rbt rbb lbb lbt -- orange
-    ]
+    let
+        rft =
+            vec3 1 1 1
+
+        lft =
+            vec3 -1 1 1
+
+        lbt =
+            vec3 -1 -1 1
+
+        rbt =
+            vec3 1 -1 1
+
+        rbb =
+            vec3 1 -1 -1
+
+        rfb =
+            vec3 1 1 -1
+
+        lfb =
+            vec3 -1 1 -1
+
+        lbb =
+            vec3 -1 -1 -1
+    in
+    WebGL.triangles <|
+        List.concat <|
+            [ face (vec3 115 210 22) rft rfb rbb rbt -- green
+            , face (vec3 52 101 164) rft rfb lfb lft -- blue
+            , face (vec3 237 212 0) rft lft lbt rbt -- yellow
+            , face (vec3 204 0 0) rfb lfb lbb rbb -- red
+            , face (vec3 117 80 123) lft lfb lbb lbt -- purple
+            , face (vec3 245 121 0) rbt rbb lbb lbt -- orange
+            ]
 
 
 face : Vec3 -> Vec3 -> Vec3 -> Vec3 -> Vec3 -> List ( Vertex, Vertex, Vertex )
 face color a b c d =
-  let
-    vertex position =
-      Vertex (Vec3.scale (1 / 255) color) position
-  in
-  [ ( vertex a, vertex b, vertex c )
-  , ( vertex c, vertex d, vertex a )
-  ]
+    let
+        vertex position =
+            Vertex (Vec3.scale (1 / 255) color) position
+    in
+    [ ( vertex a, vertex b, vertex c )
+    , ( vertex c, vertex d, vertex a )
+    ]
 
 
 
@@ -139,7 +157,7 @@ face color a b c d =
 
 vertexShader : WebGL.Shader Vertex Uniforms { vcolor : Vec3 }
 vertexShader =
-  [glsl|
+    [glsl|
     attribute vec3 position;
     attribute vec3 color;
     uniform mat4 perspective;
@@ -155,7 +173,7 @@ vertexShader =
 
 fragmentShader : WebGL.Shader {} Uniforms { vcolor : Vec3 }
 fragmentShader =
-  [glsl|
+    [glsl|
     precision mediump float;
     varying vec3 vcolor;
     void main () {
