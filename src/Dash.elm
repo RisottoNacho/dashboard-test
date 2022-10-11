e68b72c4d2078a4def0212ef2b62c548c46c9ef2
Ignacio López Ares <47207829+RisottoNacho@users.noreply.github.com>
2022-10-11 14:02:32 +0200

    Changed test to a form input

--- src/Dash.elm
+++ src/Dash.elm
@@ -1,12 +1,9 @@
 module Dash exposing (..)
 
 import Browser
-import Browser.Events as E
-import Html exposing (Html)
-import Html.Attributes exposing (height, style, width)
-import Math.Matrix4 as Mat4 exposing (Mat4)
-import Math.Vector3 as Vec3 exposing (Vec3, vec3)
-import WebGL
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 
 
@@ -14,12 +11,7 @@ import WebGL
 
 
 main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+    Browser.element { init = init, subscriptions = \_ -> Sub.none, update = update, view = view }
 
 
 
@@ -27,12 +19,15 @@ main =
 
 
 type alias Model =
-    Float
+    { name : String
+    , password : String
+    , passwordAgain : String
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( 0, Cmd.none )
+    ( Model "" "" "", Cmd.none )
 
 
 
@@ -40,23 +35,26 @@ init () =
 
 
 type Msg
-    = TimeDelta Float
+    = Name String
+    | Password String
+    | PasswordAgain String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg angle =
-    case msg of
-        TimeDelta dt ->
-            ( angle + dt / 5000, Cmd.none )
+update msg model =
+    let
+        newModel =
+            case msg of
+                Name name ->
+                    { model | name = name }
 
+                Password password ->
+                    { model | password = password }
 
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    E.onAnimationFrameDelta TimeDelta
+                PasswordAgain password ->
+                    { model | passwordAgain = password }
+    in
+    ( newModel, Cmd.none )
 
 
 
@@ -64,119 +62,31 @@ subscriptions _ =
 
 
 view : Model -> Html Msg
-view angle =
-    WebGL.toHtml
-        [ width 400
-        , height 400
-        , style "display" "block"
+view model =
+    div [ class " flex flex-row items-center " ]
+        [ viewInput "text" "Name" model.name Name
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewValidation model
         ]
-        [ WebGL.entity vertexShader fragmentShader cubeMesh (uniforms angle)
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    input
+        [ class " text-black border rounded w-64 py-2 px-3 leading-tight focus:outline-none"
+        , type_ t
+        , placeholder p
+        , value v
+        , onInput toMsg
         ]
+        []
 
 
-type alias Uniforms =
-    { rotation : Mat4
-    , perspective : Mat4
-    , camera : Mat4
-    }
+viewValidation : Model -> Html msg
+viewValidation model =
+    if model.password == model.passwordAgain then
+        div [ style "color" "green" ] [ text "OK" ]
 
-
-uniforms : Float -> Uniforms
-uniforms angle =
-    { rotation =
-        Mat4.mul
-            (Mat4.makeRotate (3 * angle) (vec3 0 1 0))
-            (Mat4.makeRotate (2 * angle) (vec3 1 0 0))
-    , perspective = Mat4.makePerspective 45 1 0.01 100
-    , camera = Mat4.makeLookAt (vec3 0 0 5) (vec3 0 0 0) (vec3 0 1 0)
-    }
-
-
-
--- MESH
-
-
-type alias Vertex =
-    { color : Vec3
-    , position : Vec3
-    }
-
-
-cubeMesh : WebGL.Mesh Vertex
-cubeMesh =
-    let
-        rft =
-            vec3 1 1 1
-
-        lft =
-            vec3 -1 1 1
-
-        lbt =
-            vec3 -1 -1 1
-
-        rbt =
-            vec3 1 -1 1
-
-        rbb =
-            vec3 1 -1 -1
-
-        rfb =
-            vec3 1 1 -1
-
-        lfb =
-            vec3 -1 1 -1
-
-        lbb =
-            vec3 -1 -1 -1
-    in
-    WebGL.triangles <|
-        List.concat <|
-            [ face (vec3 115 210 22) rft rfb rbb rbt -- green
-            , face (vec3 52 101 164) rft rfb lfb lft -- blue
-            , face (vec3 237 212 0) rft lft lbt rbt -- yellow
-            , face (vec3 204 0 0) rfb lfb lbb rbb -- red
-            , face (vec3 117 80 123) lft lfb lbb lbt -- purple
-            , face (vec3 245 121 0) rbt rbb lbb lbt -- orange
-            ]
-
-
-face : Vec3 -> Vec3 -> Vec3 -> Vec3 -> Vec3 -> List ( Vertex, Vertex, Vertex )
-face color a b c d =
-    let
-        vertex position =
-            Vertex (Vec3.scale (1 / 255) color) position
-    in
-    [ ( vertex a, vertex b, vertex c )
-    , ( vertex c, vertex d, vertex a )
-    ]
-
-
-
--- SHADERS
-
-
-vertexShader : WebGL.Shader Vertex Uniforms { vcolor : Vec3 }
-vertexShader =
-    [glsl|
-    attribute vec3 position;
-    attribute vec3 color;
-    uniform mat4 perspective;
-    uniform mat4 camera;
-    uniform mat4 rotation;
-    varying vec3 vcolor;
-    void main () {
-        gl_Position = perspective * camera * rotation * vec4(position, 1.0);
-        vcolor = color;
-    }
-  |]
-
-
-fragmentShader : WebGL.Shader {} Uniforms { vcolor : Vec3 }
-fragmentShader =
-    [glsl|
-    precision mediump float;
-    varying vec3 vcolor;
-    void main () {
-        gl_FragColor = 0.8 * vec4(vcolor, 1.0);
-    }
-  |]
+    else
+        div [ style "color" "red" ] [ text "Passwords do not match!" ]
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './main.css';
 import { Elm } from './Dash.elm';
 
 Elm.Dash.init({
-  node: document.getElementById('elm-node')
+  node: document.getElementById('root')
 });
 
 // If you want your app to work offline and load faster, you can change
